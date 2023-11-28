4177418253769ed647622f64d9dfa61b2963a7dc
abdulloh0512 <abdullohdev0512@gmail.com>
2023-11-28 21:45:07 +0500

    sdf

---
+++ script.js
@@ -0,0 +1,184 @@
+// 1.charAt()
+
+// let a = "Salom Abdulloh";
+// String.prototype.charAt = function (el) {
+//   for (let i = 0; i < this.length; i++) {
+//     if (this[i] == el) {
+//       this[i];
+//     }
+//   }
+// };
+// console.log(a.charAt(0))
+
+// 2.concat()
+
+// let a = "Salom Abdulloh";
+
+// String.prototype.concat = function (el) {
+//   let res = `${this}${el}`;
+// };
+
+// console.log(a.concat(1));
+
+// 3.endsWith ()
+// String.prototype.endsWith = function (el) {
+//   if (this == el) {
+//     ("true");
+//   } else {
+// ("false");
+//   }
+// };*
+
+// console.log(a.endsWith(1));
+
+// 4.includes ()
+// String.prototype.includes = function () {
+//   if (this == "") {
+//     true;
+//   } else {
+//     false;
+//   }
+// };
+// console.log(a.includes("a"));
+
+// 5.indexOf()
+
+// String.prototype.indexOf = function (el) {
+//   for (let i = 0; i < this.length; i++) {
+//     if (this[i] == el) {
+//       this[i];
+//     }
+//   }
+// };
+// console.log(a.indexOf("a"));
+
+// 6. charCodeAt()
+// function getUnicodeValueAt(string, index) {
+//     return string.charCodeAt(index);
+//  }
+
+// 7. codePointAt()
+//  function getCodePointAt(string, index) {
+//     return string.codePointAt(index);
+//  }
+
+// 8. lastIndexOf()
+//  function findLastOccurrence(string, searchValue) {
+//     return string.lastIndexOf(searchValue);
+//  }
+
+// 9. localeCompare()
+//  function compareLocale(stringA, stringB) {
+//     return stringA.localeCompare(stringB);
+//  }
+
+// 10. match()
+//  function findMatch(string, pattern) {
+//     return string.match(pattern);
+//  }
+
+//  11. matchAll()
+//  function findAllMatches(string, pattern) {
+//     let matches = [...string.matchAll(pattern)];
+//     return matches.map(match => match[0]);
+//  }
+
+// 12. normalize()
+//  function normalizeString(string) {
+//     return string.normalize();
+//  }
+
+// 13. padEnd()
+//  function padEndString(string, targetLength, padString) {
+//     return string.padEnd(targetLength, padString);
+//  }
+
+//  14. padStart()
+//  function padStartString(string, targetLength, padString) {
+//     return string.padStart(targetLength, padString);
+//  }
+
+// 15. repeat()
+//  function repeatString(string, count) {
+//     return string.repeat(count);
+//  }
+
+//  16. replace()
+//  function replaceSubstring(string, pattern, replacement) {
+//     return string.replace(pattern, replacement);
+//  }
+
+// 17. search()
+//  function findPattern(string, pattern) {
+//     return string.search(pattern);
+//  }
+
+// 18. slice()
+//  function sliceString(string, start, end) {
+//     return string.slice(start, end);
+//  }
+
+//  19. split()
+//  function splitString(string, separator) {
+//     return string.split(separator);
+//  }
+
+// 20. startsWith()
+//  function startsWithSubstring(string, searchValue) {
+//     return string.startsWith(searchValue);
+//  }
+
+//  21. valueOf()
+//  function getValueOf(string) {
+//     return string.valueOf();
+//  }
+// 22. substring()
+//  function getSubstring(string, start, end) {
+//     return string.substring(start, end);
+//  }
+
+//  23. substring()
+//  function getSubstringAlt(string, start, end) {
+//     return string.substring(start, end);
+//  }
+
+// 24. toLocaleLowerCase()
+//  function convertToLocaleLowerCase(string) {
+//     return string.toLocaleLowerCase();
+//  }
+
+//  25. toLocaleUpperCase()
+//  function convertToLocaleUpperCase(string) {
+//     return string.toLocaleUpperCase();
+//  }
+
+//  26. toLowerCase()
+//  function convertToLowerCase(string) {
+//     return string.toLowerCase();
+//  }
+
+//  27. toString()
+//  function convertToString(value) {
+//     return value.toString();
+//  }
+
+// 28. toUpperCase()
+//  function convertToUpperCase(string) {
+//     return string.toUpperCase();
+//  }
+
+// 29. trim()
+//  function trimString(string) {
+//     return string.trim();
+//  }
+
+//  30. trimEnd()
+//  function trimEndString(string) {
+//     return string.trimEnd();
+//  }
+
+//  31. trimStart()
+//  function trimStartString(string) {
+//     return string.trimStart();
+//  }
+
